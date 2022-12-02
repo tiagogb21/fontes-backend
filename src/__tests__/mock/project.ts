19e8fe993ccaa9c11342330ed4b2154d8feddc83
tiago garbi <tgbmateriais@gmail.com>
2022-12-02 18:31:08 -0300

    feat: :white_check_mark: add mock to test

---
+++ src/__tests__/mock/project.ts
@@ -0,0 +1,9 @@
+import { formatedDate } from "../../utils/data/functions/functions";
+
+export const projectOne = {
+  title: 'Nome do projeto',
+  zip_code: 88010400,
+  cost: 9500,
+  deadline: formatedDate('2022-09-31T 00:00:00.000Z'),
+  username: 'joao.silva',
+}
---
+++ src/__tests__/mock/user.ts
@@ -0,0 +1,11 @@
+export const userOne = {
+  name: "João Silva",
+  username: "joao.silva",
+  password: "12345678",
+};
+
+export const userTwo = {
+  name: "João Ninguem",
+  username: "joao.ninguem",
+  password: "12345678",
+};
