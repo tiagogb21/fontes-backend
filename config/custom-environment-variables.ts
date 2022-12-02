b1b9623cf1ecfbaed489c57b12780168e4f3f96e
tiago garbi <tgbmateriais@gmail.com>
2022-12-02 18:31:32 -0300

    feat: :wrench: add configuration file

---
+++ config/custom-environment-variables.ts
@@ -0,0 +1,17 @@
+export default {
+  port: 'PORT',
+
+  postgresConfig: {
+    host: 'POSTGRES_HOST',
+    port: 'POSTGRES_PORT',
+    username: 'POSTGRES_USER',
+    password: 'POSTGRES_PASSWORD',
+    database: 'POSTGRES_DB',
+  },
+
+  jwtSecret: 'JWT_SECRET',
+  accessTokenPrivateKey: 'JWT_ACCESS_TOKEN_PRIVATE_KEY',
+  accessTokenPublicKey: 'JWT_ACCESS_TOKEN_PUBLIC_KEY',
+  refreshTokenPrivateKey: 'JWT_REFRESH_TOKEN_PRIVATE_KEY',
+  refreshTokenPublicKey: 'JWT_REFRESH_TOKEN_PUBLIC_KEY',
+};
---
+++ config/default.ts
@@ -0,0 +1,6 @@
+export default {
+  origin: 'http://localhost:8080',
+  accessTokenExpiresIn: 15,
+  refreshTokenExpiresIn: 60,
+  redisCacheExpiresIn: 60,
+};
